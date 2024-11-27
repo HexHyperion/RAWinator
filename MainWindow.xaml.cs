6308197592785ee738336cd611a235a9c9298c61
Szymon <szymonjan@gmail.com>
2024-11-27 09:04:58 +0100

    Small test that does work, todo figure out color space/dynamic range

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Drawing;
 using Sdcb.LibRaw;
 using System.IO;
 using static System.Net.Mime.MediaTypeNames;
+using Sdcb.LibRaw.Natives;
 
 namespace rawinator
 {
@@ -20,15 +21,37 @@ namespace rawinator
     /// </summary>
     public partial class MainWindow : Window
     {
+        RawContext r = RawContext.OpenFile("C:\\Users\\szymo\\source\\repos\\HexHyperion\\rawinator\\img\\DSC6717.NEF");
+
         public MainWindow()
         {
             InitializeComponent();
-            RawContext raw = RawContext.OpenFile("C:\\Users\\szymo\\source\\repos\\HexHyperion\\rawinator\\img\\DSC6717.NEF");
-            ProcessedImage thumb = raw.ExportThumbnail();
-            Console.WriteLine("hej 1");
-            Bitmap bmp = (Bitmap)Bitmap.FromStream(new MemoryStream(thumb.AsSpan<byte>().ToArray()));
-            Console.WriteLine("hej 2");
+
+            r.Unpack();
+            r.DcrawProcess();
+            ProcessedImage image = r.MakeDcrawMemoryImage();
+            Bitmap bmp = ProcessedImageToBitmap(image);
             imaze.Source = BitmapToImageSource(bmp);
+
+            LibRawImageParams imageParams = r.ImageParams;
+            LibRawImageOtherParams otherParams = r.ImageOtherParams;
+            LibRawLensInfo lensInfo = r.LensInfo;
+
+            meta.Content += ($"Camera: {imageParams.Model}\n");
+            meta.Content += ($"Version: {imageParams.Software}\n");
+            meta.Content += ($"ISO: {otherParams.IsoSpeed}\n");
+            meta.Content += ($"Shutter Speed: 1/{1 / otherParams.Shutter:F0}s\n");
+            meta.Content += ($"Focal Length: {otherParams.FocalLength}mm\n");
+            meta.Content += ($"Artist Tag: {otherParams.Artist}\n");
+            meta.Content += ($"Shot Date: {new DateTime(1970, 1, 1, 8, 0, 0).AddSeconds(otherParams.Timestamp)}\n");
+            meta.Content += ($"Lens Name: {lensInfo.Lens}");
+        }
+
+        Bitmap ProcessedImageToBitmap(ProcessedImage rgbImage)
+        {
+            rgbImage.SwapRGB();
+            using Bitmap bmp = new Bitmap(rgbImage.Width, rgbImage.Height, rgbImage.Width * 3, System.Drawing.Imaging.PixelFormat.Format24bppRgb, rgbImage.DataPointer);
+            return new Bitmap(bmp);
         }
 
         BitmapImage BitmapToImageSource(Bitmap bitmap)
@@ -45,6 +68,36 @@ namespace rawinator
 
                 return bitmapimage;
             }
+        }
+
+        public static Bitmap ByteToImage(byte[] blob)
+        {
+            using (MemoryStream mStream = new MemoryStream())
+            {
+                mStream.Write(blob, 0, blob.Length);
+                mStream.Seek(0, SeekOrigin.Begin);
+
+                Bitmap bm = new Bitmap(mStream);
+                return bm;
+            }
+        }
+
+
+        private void expo_Click(object sender, RoutedEventArgs e)
+        {
+            r.DcrawProcess(c =>
+            {
+                c.HalfSize = true;
+                c.Brightness = 2.5F;
+            });
+            using ProcessedImage rgbImage = r.MakeDcrawMemoryImage();
+            Bitmap bmp = ProcessedImageToBitmap(rgbImage);
+            imaze.Source = BitmapToImageSource(bmp);
+        }
+
+        private void export_Click(object sender, RoutedEventArgs e)
+        {
+            r.SaveRawImage("C:\\Users\\szymo\\Desktop\\DSC6717.tiff");
         }
     }
 }
