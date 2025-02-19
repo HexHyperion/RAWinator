a6ed02d10ccf22305199cd10b8e8058e7bfaa178
Szymon <szymonjan@gmail.com>
2025-02-19 11:17:28 +0100

    Start working on the UI, add top menu, tabs and a bit of library

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using Sdcb.LibRaw;
 using System.IO;
 using static System.Net.Mime.MediaTypeNames;
 using Sdcb.LibRaw.Natives;
+using Microsoft.Win32;
 
 namespace rawinator
 {
@@ -27,25 +28,9 @@ namespace rawinator
         {
             InitializeComponent();
 
-            r.Unpack();
-            r.DcrawProcess();
-            ProcessedImage image = r.MakeDcrawMemoryImage();
-            Bitmap bmp = ProcessedImageToBitmap(image);
-            imaze.Source = BitmapToImageSource(bmp);
-
-            LibRawImageParams imageParams = r.ImageParams;
-            LibRawImageOtherParams otherParams = r.ImageOtherParams;
-            LibRawLensInfo lensInfo = r.LensInfo;
-
-            meta.Content += ($"Camera: {imageParams.Model}\n");
-            meta.Content += ($"Version: {imageParams.Software}\n");
-            meta.Content += ($"ISO: {otherParams.IsoSpeed}\n");
-            meta.Content += ($"Shutter Speed: 1/{1 / otherParams.Shutter:F0}s\n");
-            meta.Content += ($"Focal Length: {otherParams.FocalLength}mm\n");
-            meta.Content += ($"Artist Tag: {otherParams.Artist}\n");
-            meta.Content += ($"Shot Date: {new DateTime(1970, 1, 1, 8, 0, 0).AddSeconds(otherParams.Timestamp)}\n");
-            meta.Content += ($"Lens Name: {lensInfo.Lens}");
+            
         }
+
 
         Bitmap ProcessedImageToBitmap(ProcessedImage rgbImage)
         {
@@ -82,22 +67,46 @@ namespace rawinator
             }
         }
 
-
-        private void expo_Click(object sender, RoutedEventArgs e)
+        private void Library_Import_Click(object sender, RoutedEventArgs e)
         {
-            r.DcrawProcess(c =>
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "RAW files|*.arw;*.cr2;*.cr3;*.nef;*.nrw;*.orf;*.pef;*.raf;*.rw2;*.srw;*.dng;*.k25;*.kdc;*.srf;*.sr2;*.mos;*.3fr;*.fff;*.rwl;*.iiq";
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == true)
             {
-                c.HalfSize = true;
-                c.Brightness = 2.5F;
-            });
-            using ProcessedImage rgbImage = r.MakeDcrawMemoryImage();
-            Bitmap bmp = ProcessedImageToBitmap(rgbImage);
-            imaze.Source = BitmapToImageSource(bmp);
+                // foreach here add to list
+            }
         }
 
-        private void export_Click(object sender, RoutedEventArgs e)
+
+        private void Menu_File_Open_Click(object sender, RoutedEventArgs e)
         {
-            r.SaveRawImage("C:\\Users\\szymo\\Desktop\\DSC6717.tiff");
+
+        }
+
+        private void Menu_File_Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_File_Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Edit_Undo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Edit_Redo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Help_About_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
